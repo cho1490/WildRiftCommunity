fe25300a67bb23c98467f93f7044f03d190a20b6
cho1490 <chosanghyun99@gmail.com>
2020-11-10 03:01:51 +0900

    Add PostInfo Acitivty

--- app/src/main/java/com/example/wildriftcommunity/post/view/PostInfoActivity.kt
+++ app/src/main/java/com/example/wildriftcommunity/post/view/PostInfoActivity.kt
@@ -2,11 +2,59 @@ package com.example.wildriftcommunity.post.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.example.wildriftcommunity.ProgressListener
 import com.example.wildriftcommunity.R
+import com.example.wildriftcommunity.data.models.Post
+import com.example.wildriftcommunity.databinding.ActivityPostInfoBinding
+import com.example.wildriftcommunity.post.viewmodel.PostViewModel
+import com.example.wildriftcommunity.post.viewmodel.PostViewModelFactory
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.kodein
+import org.kodein.di.generic.instance
 
-class PostInfoActivity : AppCompatActivity() {
+class PostInfoActivity : AppCompatActivity(), ProgressListener, KodeinAware {
+
+    override val kodein by kodein()
+    private val factory: PostViewModelFactory by instance()
+    private lateinit var postViewModel: PostViewModel
+    private lateinit var binding : ActivityPostInfoBinding
+
+    var postData: Post? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_post_info)
+
+        postData = intent.getParcelableExtra<Post>("postData")
+
+        postViewModel = ViewModelProvider(this, factory).get(PostViewModel::class.java)
+        postViewModel.progressListener = this
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_post_info)
+        binding.profileViewModel = postViewModel
+        binding.lifecycleOwner = this
+
+        binding.apply {
+            postType.text = postData!!.type + "게시판"
+            postTitle.text = postData!!.title
+            postBody.text = postData!!.body
+            Glide.with(this.root).load(postData!!.imageUrl).into(postImage)
+        }
+
+    }
+
+    override fun onStarted() {
+        binding.progressbarPostInfo.visibility = View.VISIBLE
+    }
+
+    override fun onSuccess(message: String) {
+        binding.progressbarPostInfo.visibility = View.GONE
+    }
+
+    override fun onFailure(message: String) {
+        binding.progressbarPostInfo.visibility = View.GONE
     }
 }
